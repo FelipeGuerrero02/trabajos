61779cbb7eb0082c395a3fdb7d65684c32a62b0f
adrian <ag4931249@gmail.com>
2024-11-28 19:36:05 -0500

    nombre,edad,inicial.mayordeedad

---
+++ NEIM.java
@@ -0,0 +1,13 @@
+class NEIM {
+    public static void main (String[] args){
+        String nombre = "pipe";
+        int edad = 19;
+        char inicial = 'p';
+        boolean mayordeedad= true;
+
+        System.out.println("nombre"+ nombre);
+        System.out.println("edad : " + edad);
+        System.out.println("inicial : " + inicial);
+        System.out.println("mayordeedad: " + mayordeedad);
+    }
+}
