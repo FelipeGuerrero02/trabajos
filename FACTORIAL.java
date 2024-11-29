dc6622eb4db7ebed692c532614d086745e3871da
adrian <ag4931249@gmail.com>
2024-11-29 12:08:01 -0500

    factorial de un numero

---
+++ FACTORIAL.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FACTORIAL {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        int factorial = 1;
+        int i = 1;
+        while (i <= numero) {
+            factorial *= i;
+            i++;
+        }
+
+        System.out.println("El factorial de " + numero + " es: " + factorial);
+    }
+}
